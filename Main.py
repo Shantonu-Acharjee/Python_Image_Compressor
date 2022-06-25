efa7693e42014c4aa9b40f125e98fce1ffe7dac3
Shantonu Acharjee <shantonuacharjee@gmail.com>
2022-06-25 03:26:57 -0700

    ALL Done

--- Main.py
+++ Main.py
@@ -7,14 +7,34 @@ PhotoDir = (input('Enter Your Folder Path : ')+ '/')
 Photos = os.listdir(PhotoDir)
 print(Photos)
 a = len(Photos)
-os.makedirs(PhotoDir+'Compress Photo')
+
+if "Compress Photo" not in Photos:
+    os.makedirs(PhotoDir+'Compress Photo')
+
+else:
+    a = a - 1
+
+CompressImagePath = os.listdir(PhotoDir+'Compress Photo' + '/')
+
+
 
 for i in range(0, a):
     try:
 
-        if '.jpeg' in Photos[i] or '.jpg' in Photos[i] or '.png' in Photos[i] or '.gif' in Photos[i] or '.raw' in Photos[i]:
+        if '.jpeg' in Photos[i] or '.JPEG' in Photos[i] or  '.jpg' in Photos[i] or '.JPG' in Photos[i] or '.png' in Photos[i] or '.PNG' in Photos[i] or '.gif' in Photos[i] or '.GIF' in Photos[i] or '.raw' in Photos[i] or '.RAW' in Photos[i]:
             
             image = PIL.Image.open(PhotoDir+Photos[i])
+
+            try:
+
+                if Photos[i] in CompressImagePath[i]:
+                   continue
+
+            except:
+                print(Photos[i] + ' Not in Compress Photo')
+            
+
+            
             width, height = image.size
             print(Photos[i], '|',width,',', height, '|')
             persentage = height / width
